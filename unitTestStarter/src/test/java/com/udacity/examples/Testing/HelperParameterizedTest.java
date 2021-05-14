0789399027af854d00d094e9fcd3c990941f9ee5
Plora Tran <ploratran@gmail.com>
2021-05-14 11:44:57 -0700

    code cleaning on Paramterized method

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperParameterizedTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperParameterizedTest.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.assertNotEquals;
 /**
  * Parameterized Tests allow you to run the SAME test again
  * using different values
+ * Reference: https://github.com/junit-team/junit4/wiki/Parameterized-tests
+ * Reference: https://www.tutorialspoint.com/junit/junit_parameterized_test.htm
  * */
 @RunWith(Parameterized.class) // define class to be Parameterized test class
 public class HelperParameterizedTest {
@@ -19,6 +21,7 @@ public class HelperParameterizedTest {
     private String input;
     private String output;
 
+    // initialize Constructor of the class:
     public HelperParameterizedTest(String input, String output) {
         super();
         this.input = input;
@@ -27,10 +30,9 @@ public class HelperParameterizedTest {
 
     // serve as a data provider:
     @Parameterized.Parameters
-    public static Collection initData() {
-        String empNames[][] = {{"plora", "plora"}, {"plora", "Jeff"}};
-        // return as a Collection:
-        return Arrays.asList(empNames);
+    public static Collection<String[]> initData() {
+        // Return as a Collection of String[]:
+        return Arrays.asList(new String[][]{{"plora", "plora"}, {"plora", "Jeff"}});
     }
 
     // without parameters:
